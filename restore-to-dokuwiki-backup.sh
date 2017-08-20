a2ba62bb9cef3a9c272bfada5bd35309b3602716
yqsy <yqsy021@126.com>
2017-08-20 20:37:22 +0800

    update

--- restore-to-dokuwiki-backup.sh
+++ restore-to-dokuwiki-backup.sh
@@ -1,1 +1,13 @@
-echo $1
+docker run \
+  -v /dokuwiki/data/ \
+  -v /dokuwiki/lib/plugins/ \
+  -v /dokuwiki/conf/ \
+  -v /dokuwiki/lib/tpl/ \
+  -v /var/log/ \
+--name dokuwiki-backup ubuntu:14.04
+
+docker run --rm --volumes-from dokuwiki-backup \
+  -v $(pwd):/backup \
+  ubuntu:14.04 \
+  bash -c "cd / && tar -xvzf /$1"
+  
